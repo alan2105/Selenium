0d544e8dbf4a80039cb430a947114887a3e7133e
Alagappan <alagappanmca21@gmail.com>
2017-06-08 18:12:26 +0530

    Data driven script updated

--- Sample/src/com/datadrivenframework/test/ddf1.java
+++ Sample/src/com/datadrivenframework/test/ddf1.java
@@ -2,7 +2,7 @@ package com.datadrivenframework.test;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
