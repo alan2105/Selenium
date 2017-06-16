d29be30c4ee210dbb52a7a723675001d6f443305
Alagappan <alagappanmca21@gmail.com>
2017-06-16 18:14:01 +0530

    Commit after update of Data driven script

--- Sample/src/com/datadrivenframework/test/ddf1.java
+++ Sample/src/com/datadrivenframework/test/ddf1.java
@@ -2,23 +2,25 @@ package com.datadrivenframework.test;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import lib.ExcelFileConfig;
 
 public class ddf1 {
 
 	WebDriver driver;
-	String appurl = "http://test.vzone.vmokshagroup.com/";
+	String appurl = "http://biomagnetictherapy-stg.us-west-2.elasticbeanstalk.com/Dashboard";
 	@Test(dataProvider = "testdata")
-	public void TestVzoneLogin(String uname,String pass) throws InterruptedException 
+	public void TestVzoneLogin(String uname,String pass) throws Exception 
 	{
-		System.setProperty("webdriver.gecko.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/GeckoDriver/geckodriver-v0.16.0-win64/geckodriver.exe");
-		driver = new FirefoxDriver();
-		//System.setProperty("webdriver.chrome.driver", "D:eclipse/java-neon/eclipse/Third party drivers/ChromeDriver/chromedriver_win32_2.29/chromedriver.exe");
-		//driver = new ChromeDriver();
+		//System.setProperty("webdriver.gecko.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/GeckoDriver/geckodriver-v0.16.0-win64/geckodriver.exe");
+		//driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "D:eclipse/java-neon/eclipse/Third party drivers/ChromeDriver/chromedriver_win32_2.29/chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.get(appurl);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -27,29 +29,40 @@ public class ddf1 {
 		driver.findElement(By.xpath(".//*[@id='btnSignIn']")).click();
 		Thread.sleep(5000);
 		//driver.findElement(By.id("btnClear")).click();
+		
 	
 		System.out.println("Application Title is:" + driver.getTitle());
 		
-		Assert.assertTrue(driver.getTitle().contains("Dashboard"),"User not able to login. Invaild Credentials");
-		
+		Assert.assertTrue(driver.getCurrentUrl().contains("Dashboard"),"User not able to login. Invaild Credentials");
+	
 		System.out.println("Page title verified. User able to login successfully");
 		
-		Thread.sleep(2000);
-		driver.quit();
-		
+		Thread.sleep(2000);	
+	}
+	
+	@AfterMethod
+	public void TearDown() {
+
+		driver.close();
 	}
 	@DataProvider (name = "testdata")
-	public Object[][] TestDataFeed()
+	public Object[][] TestDataFeed() throws Exception
+	
 	{
-		Object[][] data = new Object[2][2];
+		ExcelFileConfig config = new ExcelFileConfig("C:/Users/alagappan.n/git/Selenium-code/Sample/TestData/testdata1.xlsx");
 		
-		data[0][0] = "alagappan.n@vmokshagroup.com";
-		data[0][1] = "Power@1234";
+		int rows = config.getRowCount(0);
+		Object[][] data = new Object[rows][2];
 		
-		data[1][0] = "alagappan.n@vmokshagroup.com";
-		data[1][1] = "alan2119";
-		
+		for(int i=1;i<rows;i++)
+		{
+			data[i][0] = config.getData(0, i, 0);
+			data[i][1] = config.getData(0, i, 1);
+					
+		}
 		return data;
-		
 	}
+	
+	
+	
 }
