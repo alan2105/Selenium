0252a2565230ea05aab0359430fa7675436e6574
Alagappan <alagappanmca21@gmail.com>
2017-09-28 18:16:11 +0530

    Committed on 28-Sep-2017

--- Selenium_Cucumber_Projects/src/stepDefnition/smokeTest.java
+++ Selenium_Cucumber_Projects/src/stepDefnition/smokeTest.java
@@ -3,6 +3,7 @@ package stepDefnition;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -15,16 +16,18 @@ public class smokeTest {
 	@Given("^Open chrome and start application$")
 	public void Open_chrome_and_start_application() throws Throwable {
 		
-	System.setProperty("webdriver.chrome.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/ChromeDriver/chromedriver_win32_2.29/chromedriver.exe");
-	driver = new ChromeDriver();
+//	System.setProperty("webdriver.chrome.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/ChromeDriver/chromedriver_win32_2.29/chromedriver.exe");
+//	driver = new ChromeDriver();
+	System.setProperty("webdriver.gecko.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/GeckoDriver/geckodriver-v0.16.0-win64/geckodriver.exe");
+	driver = new FirefoxDriver();
 	driver.manage().window().maximize();
 	driver.get("http://test.vzone.vmokshagroup.com/");
 	}
 
 	@When("^I enter valid user name and password$")
 	public void I_enter_valid_user_name_and_password() throws Throwable {
-	    driver.findElement(By.id("inputUsername")).sendKeys("alagappan.n@vmokshagroup.com");;
-	    driver.findElement(By.id("inputPassword")).sendKeys("Power@1234");
+	    driver.findElement(By.xpath(".//*[@id='inputUsername']")).sendKeys("alagappan.n@vmokshagroup.com");
+	    driver.findElement(By.xpath(".//*[@id='inputPassword']")).sendKeys("Power@1234");
 	}
 
 	@Then("^User should be able to log	in$")
