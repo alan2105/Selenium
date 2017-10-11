91744214dcb78e4a85f00af59b3723f155a9e644
Alagappan <alagappanmca21@gmail.com>
2017-10-11 16:07:53 +0530

    Commit on 11-Sep-2017

---
+++ JenkinsDemo/src/JenkinsProjects/SampleDemo.java
@@ -0,0 +1,13 @@
+package JenkinsProjects;
+
+import org.testng.annotations.Test;
+
+public class SampleDemo {
+	
+	@Test
+	public void TestJenkins()
+	{
+		System.out.println("Welcome to the jenkins world..!");
+	}
+
+}
