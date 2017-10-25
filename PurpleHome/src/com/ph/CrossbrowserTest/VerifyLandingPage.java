ab2cf277c455b22ea8567b8bdd16d4f3159fabec
Alagappan <alagappanmca21@gmail.com>
2017-10-25 18:01:56 +0530

    commit on 25-10-2017

--- PurpleHome/src/com/ph/CrossbrowserTest/VerifyLandingPage.java
+++ PurpleHome/src/com/ph/CrossbrowserTest/VerifyLandingPage.java
@@ -1,5 +1,19 @@
 package com.ph.CrossbrowserTest;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+
+
 public class VerifyLandingPage {
+	
+	static WebDriver driver;
+	static String title,url;
+	@Test
+	public static void getTitle() throws InterruptedException
+	{
+		driver.findElement(By.id("inputUsername")).sendKeys("alagappanmca21@gmail.com");
+	}
 
 }
--- PurpleHome/src/com/ph/libraries/BaseClass.java
+++ PurpleHome/src/com/ph/libraries/BaseClass.java
@@ -1,34 +1,71 @@
 package com.ph.libraries;
 
+import org.openqa.selenium.By;
+
+//import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class BaseClass {
 
 	static WebDriver driver;
-	static String appUrl = "";
-	@Test
+	static String appUrl = "http://test.vzone.vmokshagroup.com/";
 	@Parameters("Browser")
-	public static void BrowserSetUp(String browser)
+	@BeforeClass
+	public static void BrowserSetUp(String browser) throws Exception
 	{
 		if(browser.equalsIgnoreCase("Chrome"))
 		{
-		System.setProperty("webdriver.chrome.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/ChromeDriver/chroedriver_win32_2.29/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/ChromeDriver/chromedriver_win32_2.29/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(appUrl);
+		Thread.sleep(5000);
+		
+		//driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 		}
 		else if(browser.equalsIgnoreCase("Firefox"))
 		{
-			System.setProperty("webdriver.gecko.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/GeckoDriver/geckodriver-v0.16.0-win64/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/GeckoDriver/geckodriver-v0.16.0-win64/geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get(appUrl);
+		Thread.sleep(5000);
+		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		}
+		else if(browser.equalsIgnoreCase("IE"))
+		{
+		System.setProperty("webdriver.ie.driver", "D:/eclipse/java-neon/eclipse/Third party drivers/IEDriver/IEDriverServer_Win32_3.5.1/IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.get(appUrl);	
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		}
 		
+	}
+	
+	
+	@Test
+	public static void getTitle() throws InterruptedException
+	{
+		driver.findElement(By.id("inputUsername")).sendKeys("alagappanmca21@gmail.com");
+	}
+	
+	@AfterClass
+
+	public void TearDown()
+	{
+		driver.quit();
 	}
 
 }
